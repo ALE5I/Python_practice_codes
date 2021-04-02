c2f002f0ddf16e0b77b46066d42160dbf4d5b50c
Alesi L <alesi.bu.lo@gmail.com>
2021-04-02 14:13:12 -0600

    Added loop to keep playing Guess Number

--- Guess_the_number.py
+++ Guess_the_number.py
@@ -1,15 +1,21 @@
 import random
-r_number = random.randint(0,100)
 
-tries = 0
-guess = int(input("Guess the random number between 0 and 100: "))
+while True:
+    r_number = random.randint(0,100)
 
-while (guess != r_number):
-    if (guess < r_number):
-        guess = int(input("Higher: "))
-        tries += 1
-    elif (guess > r_number):
-        guess = int(input("Lower: "))
-        tries += 1
 
-print (f"You won in {tries} tries")
+    tries = 0
+    guess = int(input("Guess the random number between 0 and 100: "))
+
+    while (guess != r_number):
+        if (guess < r_number):
+            guess = int(input("Higher: "))
+            tries += 1
+        elif (guess > r_number):
+            guess = int(input("Lower: "))
+            tries += 1
+
+    print (f"You won in {tries} tries")
+    play = input("Do you want to keep playing? (y/n) ---> ")
+    if play == "n":
+        break
